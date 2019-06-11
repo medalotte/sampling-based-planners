fb6b9f13a8513045e4f94fcdc29a20f79a96b882
Yuya Kudo <kyk0910yuya@gmail.com>
2019-06-12 06:28:05 +0900

    add lib/EuclideanSpace/EuclideanSpace

---
+++ lib/include/EuclideanSpace/EuclideanSpace.h
@@ -0,0 +1,72 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Yuya Kudo
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#ifndef LIB_INCLUDE_EUCLIDEANSPACE_EUCLIDEANSPACE_H_
+#define LIB_INCLUDE_EUCLIDEANSPACE_EUCLIDEANSPACE_H_
+
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <cstdint>
+
+namespace planner {
+    class Bound {
+    public:
+        double low;
+        double high;
+
+        Bound() = default;
+        Bound(const double& low, const double& high);
+
+        Bound(const Bound& obj);
+        Bound &operator=(const Bound& obj);
+
+        ~Bound();
+
+        double getRange() const;
+    };
+
+    class EuclideanSpace {
+    public:
+        EuclideanSpace() = default;
+        EuclideanSpace(const uint32_t& dim);
+
+        EuclideanSpace(const EuclideanSpace& obj);
+        EuclideanSpace &operator=(const EuclideanSpace& obj);
+
+        ~EuclideanSpace();
+
+        uint32_t getDim() const noexcept;
+
+        void setBound(std::vector<Bound>& bounds);
+
+        Bound getBound(const uint32_t& dim) const;
+
+    private:
+        uint32_t dim_;
+        std::vector<Bound> bounds_;
+    };
+}
+
+#endif /* LIB_INCLUDE_EUCLIDEANSPACE_EUCLIDEANSPACE_H_ */
---
+++ lib/src/EuclideanSpace/EuclideanSpace.cpp
@@ -0,0 +1,110 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Yuya Kudo
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#include <EuclideanSpace/EuclideanSpace.h>
+
+namespace planner {
+    Bound::Bound(const double& _low, const double& _high) {
+        if(_high < _low) {
+            throw std::runtime_error("[" + std::string(__PRETTY_FUNCTION__) + "] " +
+                                     "Bound is invalid");
+        }
+
+        high = _high;
+        low  = _low;
+    }
+
+    Bound::Bound(const Bound& obj) :
+        low(obj.low),
+        high(obj.high) {
+    }
+
+    Bound &Bound::operator =(const Bound& obj) {
+        if(this != &obj) {
+            low  = obj.low;
+            high = obj.high;
+        }
+        return *this;
+    }
+
+    Bound::~Bound() {
+    }
+
+    double Bound::getRange() const {
+        return high - low;
+    }
+
+    EuclideanSpace::EuclideanSpace(const uint32_t& dim) {
+        if(dim == 0) {
+            throw std::runtime_error("[" + std::string(__PRETTY_FUNCTION__) + "] " +
+                                     "Dimension is invalid");
+        }
+        dim_    = dim;
+        bounds_ = std::vector<Bound>(dim, Bound(0, 0));
+    }
+
+    EuclideanSpace::EuclideanSpace(const EuclideanSpace& obj) :
+        dim_(obj.getDim()) {
+        std::vector<Bound> bounds;
+        for(uint32_t di = 1; di <= dim_; di++) {
+            bounds.push_back(obj.getBound(di));
+        }
+        setBound(bounds);
+    }
+
+    EuclideanSpace &EuclideanSpace::operator=(const EuclideanSpace& obj) {
+        if(this != &obj) {
+            dim_ = obj.getDim();
+            std::vector<Bound> bounds;
+            for(uint32_t di = 1; di <= dim_; di++) {
+                bounds.push_back(obj.getBound(di));
+            }
+            setBound(bounds);
+        }
+        return *this;
+    }
+
+    EuclideanSpace::~EuclideanSpace() {
+    }
+
+    uint32_t EuclideanSpace::getDim() const noexcept {
+        return dim_;
+    }
+
+    void EuclideanSpace::setBound(std::vector<Bound>& bounds) {
+        if(bounds.size() != dim_) {
+            throw std::runtime_error("[" + std::string(__PRETTY_FUNCTION__) + "] " +
+                                     "Condition of bound is invalid");
+        }
+        bounds_ = bounds;
+    }
+
+    Bound EuclideanSpace::getBound(const uint32_t& dim) const {
+        if(!(0 < dim && dim <= dim_)) {
+            throw std::runtime_error("[" + std::string(__PRETTY_FUNCTION__) + "] " +
+                                     "Dimention is out of range");
+        }
+        return bounds_[dim - 1];
+    }
+}
