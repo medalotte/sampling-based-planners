eed3fbe90cf026e86de5ebe1c3c2b444db5410a7
Yuya Kudo <kyk0910yuya@gmail.com>
2019-06-12 06:27:51 +0900

    add lib/Constraint/SemanticSegmentConstraint/SemanticSegmentConstraint

---
+++ lib/include/Constraint/SemanticSegmentConstraint/SemanticSegmentConstraint.h
@@ -0,0 +1,61 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Yuya Kudo
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#ifndef LIB_INCLUDE_CONSTRAINT_SEMANTICSEGMENTCONSTRAINT_SEMANTICSEGMENTCONSTRAINT_H_
+#define LIB_INCLUDE_CONSTRAINT_SEMANTICSEGMENTCONSTRAINT_SEMANTICSEGMENTCONSTRAINT_H_
+
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+#include <State/State.h>
+#include <Constraint/ConstraintBase.h>
+
+namespace planner {
+    class SemanticSegmentConstraint : public base::ConstraintBase {
+    public:
+        SemanticSegmentConstraint(const EuclideanSpace& space);
+        SemanticSegmentConstraint(const EuclideanSpace&              space,
+                                  const std::vector<ConstraintType>& constraint,
+                                  const std::vector<uint32_t>&       each_dim_size);
+
+        ~SemanticSegmentConstraint() override;
+
+        void set(const std::vector<ConstraintType>& constraint,
+                 const std::vector<uint32_t>&       each_dim_size);
+
+
+        const std::vector<ConstraintType>& getConstraintRef() const;
+
+        const std::vector<uint32_t>& getEachDimSizeRef() const;
+
+        ConstraintType checkConstraintType(const State& state) const override;
+
+    private:
+        std::vector<ConstraintType> constraint_;
+        std::vector<uint32_t>       each_dim_size_;
+    };
+}
+
+#endif /* LIB_INCLUDE_CONSTRAINT_SEMANTICSEGMENTCONSTRAINT_SEMANTICSEGMENTCONSTRAINT_H_ */
---
+++ lib/src/Constraint/SemanticSegmentConstraint/SemanticSegmentConstraint.cpp
@@ -0,0 +1,82 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Yuya Kudo
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#include <Constraint/SemanticSegmentConstraint/SemanticSegmentConstraint.h>
+
+namespace planner {
+    SemanticSegmentConstraint::SemanticSegmentConstraint(const EuclideanSpace& space) :
+        base::ConstraintBase(space) {
+    }
+
+    SemanticSegmentConstraint::SemanticSegmentConstraint(const EuclideanSpace&              space,
+                                                         const std::vector<ConstraintType>& constraint,
+                                                         const std::vector<uint32_t>&       each_dim_size) :
+        base::ConstraintBase(space) {
+        set(constraint, each_dim_size);
+    }
+
+    SemanticSegmentConstraint::~SemanticSegmentConstraint() {
+    }
+
+    void SemanticSegmentConstraint::set(const std::vector<ConstraintType>& constraint,
+                                        const std::vector<uint32_t>&       each_dim_size) {
+        constraint_    = constraint;
+        each_dim_size_ = each_dim_size;
+    }
+
+    const std::vector<ConstraintType>& SemanticSegmentConstraint::getConstraintRef() const {
+        return constraint_;
+    }
+
+    const std::vector<uint32_t>& SemanticSegmentConstraint::getEachDimSizeRef() const {
+        return each_dim_size_;
+    }
+
+    ConstraintType SemanticSegmentConstraint::checkConstraintType(const State& state) const {
+        if(getDim() != state.getDim()) {
+            throw std::runtime_error("[" + std::string(__PRETTY_FUNCTION__) + "] " +
+                                     "State dimension is invalid");
+        }
+
+        // get index on single dimension array which correspond with state
+        uint32_t index = 0;
+        for(size_t i = 0; i < state.getDim(); i++) {
+            auto bound = space.getBound(i + 1);
+
+            // return NOENTRY Type if the state is out of range
+            if(state.vals[i] < bound.low || bound.high < state.vals[i]) {
+                return ConstraintType::NOENTRY;
+            }
+
+            uint32_t product = 1;
+            for(size_t j = 0; j < i; j++) {
+                product *= each_dim_size_[j];
+            }
+            index += std::floor(state.vals[i] * each_dim_size_[i] / bound.getRange()) * product;
+        }
+
+        return constraint_[index];
+    }
+}
+
